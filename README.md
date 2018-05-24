4e37f77a3c3d05d2fd05bff406bca3a2e5dc8006
Fabio Bier <fabio@proxymail.nl>
2018-05-24 23:41:39 +0200

    Initial project setup and bootstrap.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Selectron-9000
+## A better way to filter.  
+
+
+### Development setup
+To get started make sure all vendor assets are installed using `yarn`.
+```
+yarn
+```
+
+### Once the appropriate assets are installed you may now run the application.
+```
+yarn start
+```
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi there,</h1>
+        <h2>I am Selectron-9000, the ultimate multi-select.</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
